057721e991a6985cabcb95f2915b55e59acba711
bobsamuels <bsamuels@widen.com>
2017-11-29 22:38:37 -0600

    Minor tweaks to guage performance

--- routes/api/usages.js
+++ routes/api/usages.js
@@ -2,10 +2,8 @@ module.exports = function(app){
     app.post('/api/usages', function(req, res){
 
         // Store the supplied usage data
-        app.usages.push(req.body);
-
-        var usageId = app.usages.length;
-        console.log('Stored usage count: ' + usageId);
+        var usageId = app.usages.push(req.body);
+        // console.log('Stored usage count: ' + usageId);
 
         res.status(201).json({'id':usageId});
     });
